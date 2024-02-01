b466028e7ac19761e4b935dbdae1f9e9a4d3c570
Рамиль <118668474+lazybulbik@users.noreply.github.com>
2024-02-01 18:47:01 +0400

    Update YandexMapsAPI.py

--- YandexMapsAPI.py
+++ YandexMapsAPI.py
@@ -2,11 +2,16 @@ import requests
 
 
 class YandexStaticApi:
-    def get_image(self, latitude, longitude, scale):
-        coords = f'{latitude},{longitude}'
-        size = f'{scale}'
+    def __init__(self, latitude, longitude):
+        self.scale = 1
+        self.scale_step = 0.05
+        self.latitude = latitude
+        self.longitude = longitude
 
-        response = requests.get(f'https://static-maps.yandex.ru/1.x/?ll={coords}8&spn={size},0.00619&l=map')
+    def get_image(self):
+        coords = f'{self.latitude},{self.longitude}'
+
+        response = requests.get(f'https://static-maps.yandex.ru/1.x/?ll={coords}&spn={self.scale},0.00619&l=map')
 
         if response.status_code == 200:
             with open('image.png', 'wb') as f:
@@ -14,3 +19,15 @@ class YandexStaticApi:
 
             return 'ok'
         raise ValueError
+
+    def scale_up(self):
+        if self.scale == 1:
+            return
+
+        self.scale += self.scale_step
+
+    def scale_down(self):
+        if self.scale == 0.05:
+            return
+
+        self.scale -= self.scale_step
