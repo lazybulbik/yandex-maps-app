b0d460941d728718779a588c3a5adff54a9af224
TWiSTeD707 <151539904+TWiSTeD707@users.noreply.github.com>
2024-02-01 23:35:20 +0400

    Update YandexMapsAPI.py

--- YandexMapsAPI.py
+++ YandexMapsAPI.py
@@ -4,7 +4,8 @@ import requests
 class YandexStaticApi:
     def __init__(self, latitude, longitude):
         self.scale = 0.01
-        self.scale_step = 0.0005
+        self.scale_step = 0.005
+        self.move_step = 0.025
         self.latitude = latitude
         self.longitude = longitude
 
@@ -33,3 +34,31 @@ class YandexStaticApi:
 
         self.scale += self.scale_step
         print(self.scale)
+
+    def move_left(self):
+        if self.latitude - self.scale >= -180:
+            self.latitude -= self.move_step
+            print(f"New longitude: {self.longitude}")
+        else:
+            print("0")
+
+    def move_right(self):
+        if self.latitude + self.scale <= 180:
+            self.latitude += self.move_step
+            print(f"New longitude: {self.longitude}")
+        else:
+            print("0")
+
+    def move_up(self):
+        if self.longitude + self.scale <= 90:
+            self.longitude += self.move_step
+            print(f"New latitude: {self.latitude}")
+        else:
+            print("0")
+
+    def move_down(self):
+        if self.longitude - self.scale >= -90:
+            self.longitude -= self.move_step
+            print(f"New latitude: {self.latitude}")
+        else:
+            print("0")
