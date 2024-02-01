457454e9620519cf01a517642abab1e4e2acbdcf
TWiSTeD707 <151539904+TWiSTeD707@users.noreply.github.com>
2024-02-01 18:54:01 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel
 from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
 from YandexMapsAPI import YandexStaticApi
 
 
@@ -7,7 +8,19 @@ class App(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle('Map_1')
+        self.setWindowTitle('Map_2')
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_PageUp:
+            YMap.scale_up()
+            YMap.get_image()
+            self.PixMap = QPixmap('image.png')
+            self.label.setPixmap(self.PixMap)
+        elif event.key() == Qt.Key_PageDown:
+            YMap.scale_down()
+            YMap.get_image()
+            self.PixMap = QPixmap('image.png')
+            self.label.setPixmap(self.PixMap)
 
     def load_image(self, file_name):
         pixmap = QPixmap(file_name)
@@ -22,8 +35,9 @@ class App(QMainWindow):
 if __name__ == '__main__':
     import sys
 
+    YMap = YandexStaticApi(53.1738929, 56.8724243)
     app = QApplication(sys.argv)
-    YandexStaticApi().get_image(53.1738929, 56.8724243, 1)  # координаты X, Y и масштаб
+    YMap.get_image()  # координаты X, Y
     ex = App()
     ex.load_image('image.png')
     ex.show()
