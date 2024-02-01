b709191656c638edc30ebd2b31ae9ff07336e60a
TWiSTeD707 <151539904+TWiSTeD707@users.noreply.github.com>
2024-02-02 00:04:33 +0400

    Update main.py

--- main.py
+++ main.py
@@ -7,48 +7,46 @@ from YandexMapsAPI import YandexStaticApi
 class App(QMainWindow):
     def __init__(self):
         super().__init__()
-
         self.setWindowTitle('Map_2')
+        self.map_type = 'map'  # Изначально установим тип карты как "схема"
 
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_PageUp:
             YMap.scale_up()
-            YMap.get_image()
-            self.PixMap = QPixmap('image.png')
-            self.label.setPixmap(self.PixMap)
         elif event.key() == Qt.Key_PageDown:
             YMap.scale_down()
-            YMap.get_image()
-            self.PixMap = QPixmap('image.png')
-            self.label.setPixmap(self.PixMap)
         elif event.key() == Qt.Key_Up:
             YMap.move_up()
-            YMap.get_image()
-            self.PixMap = QPixmap('image.png')
-            self.label.setPixmap(self.PixMap)
         elif event.key() == Qt.Key_Down:
             YMap.move_down()
-            YMap.get_image()
-            self.PixMap = QPixmap('image.png')
-            self.label.setPixmap(self.PixMap)
         elif event.key() == Qt.Key_Left:
             YMap.move_left()
-            YMap.get_image()
-            self.PixMap = QPixmap('image.png')
-            self.label.setPixmap(self.PixMap)
         elif event.key() == Qt.Key_Right:
             YMap.move_right()
-            YMap.get_image()
-            self.PixMap = QPixmap('image.png')
-            self.label.setPixmap(self.PixMap)
+        elif event.key() == Qt.Key_M:  # Пример использования клавиши M для переключения слоев карты
+            self.change_map_layer()
+
+        YMap.get_image()
+        self.PixMap = QPixmap('image.png')
+        self.label.setPixmap(self.PixMap)
+
+    def change_map_layer(self):
+        if self.map_type == 'map':
+            self.map_type = 'sat'  # Переключаем на спутниковый вид
+        elif self.map_type == 'sat':
+            self.map_type = 'map'  # Переключаем на схему
+
+        YMap.change_map_type(self.map_type)
+
+        YMap.get_image()
+        self.PixMap = QPixmap('image.png')
+        self.label.setPixmap(self.PixMap)
 
     def load_image(self, file_name):
         pixmap = QPixmap(file_name)
-
         self.label = QLabel(self)
         self.label.setPixmap(pixmap)
         self.label.resize(pixmap.width(), pixmap.height())
-
         self.resize(pixmap.width(), pixmap.height())
 
 
